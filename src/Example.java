46b159bc23c8fc278459274b170921b915007665
Daniel <42978531+danielhan1@users.noreply.github.com>
2019-02-06 14:26:29 -0500

    Add files via upload

--- src/Example.java
+++ src/Example.java
@@ -22,8 +22,7 @@ public class Example
 			//Scanner in = new Scanner(System.in);
 			
 			JLabel lbl1 = new JLabel("Please enter your name");
-			Scanner textSca1 = new Scanner(lbl1.getText());
-			String name = lbl1.getText();
+			
 			lbl1.setBounds(15, 30, 200, 30);
 			myFrame.add(lbl1);
 			
@@ -31,33 +30,50 @@ public class Example
 			JTextField tf1 = new JTextField();
 			//String name = in.nextLine();
 			tf1.setBounds(15, 55, 200, 30);
+			//Scanner textScan1 = new Scanner(tf1.getText());
+			//String name = tf1.getText();
 			myFrame.add(tf1);
 			
+			
 			JLabel lbl2 = new JLabel("Please enter your phone number");
-			Scanner textScan2 = new Scanner(lbl2.getText());
-			String number = lbl2.getText();
+			
 			lbl2.setBounds(15, 90, 200, 30);
 			myFrame.add(lbl2);
 			
 			JTextField tf2 = new JTextField();
 			//int number = in.nextInt();
 			tf2.setBounds(15, 115, 200, 30);
+			//Scanner textScan2 = new Scanner(tf2.getText());
+			//String number = tf2.getText();
 			myFrame.add(tf2);
-			
 			
 			
 			
 			JButton button = new JButton("Command");
 			button.setSize(new Dimension(100,30));
 			button.setLocation(new Point(250, 55));
+			
+
+			JLabel lbl3 = new JLabel("Your name is: ");
+			lbl3.setBounds(250, 100, 200, 30);
+			myFrame.add(lbl3);
+			JLabel lbl4 = new JLabel("Your number is: ");
+			lbl4.setBounds(250, 150, 200, 30);
+			myFrame.add(lbl4);
+			
+			
 			button.addActionListener(new ActionListener()
 					{
 						public void actionPerformed(ActionEvent e)
 						{
-							JLabel lbl3 = new JLabel("Your name is " + name + " and your number is " + number);
-							lbl3.setBounds(100, 190, 200, 30);
-							myFrame.add(lbl3);
-							button.setLocation((int) (Math.random()*500+100), (int)(Math.random()*200+100));
+							
+							String name = tf1.getText();
+
+							String number = tf2.getText();
+							
+							lbl3.setText("Your name is: " + name);
+							lbl4.setText("Your number is: " + number);
+						
 						}
 					}
 			);
